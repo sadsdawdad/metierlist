78cad80bc88c6390ae558c31179074e2c8b89c34
sadsdawdad <streyfe8@gmail.com>
2026-03-17 22:04:38 +0500

    Add files via upload

---
+++ MEtierlist/players.js
@@ -0,0 +1,23 @@
+const players = [
+
+{name:"Mr__Pixuu", region:"AS", kits:["NethOP"], tiers:["HT5"]},
+{name:"DreamyS0ul", region:"EU", kits:["Mace"], tiers:["LT5"]},
+{name:"V_gaming_27", region:"AS", kits:["Mace"], tiers:["LT5"]},
+{name:"RaspyYT", region:"AS", kits:["Vanilla"], tiers:["HT4"]},
+{name:"Nitansh", region:"AS", kits:["Mace"], tiers:["HT5"]},
+{name:"WhiskeyyMC", region:"AS", kits:["Vanilla"], tiers:["LT3"]},
+{name:"Streyfe", region:"AS", kits:["NethOP"], tiers:["LT3"]},
+{name:"Streyfe", region:"AS", kits:["Axe"], tiers:["LT3"]},
+{name:"Agamer941", region:"AS", kits:["NethOP"], tiers:["HT4"]},
+{name:"Agamer941", region:"AS", kits:["Sword"], tiers:["HT5"]},
+{name:"Streyfe", region:"AS", kits:["Sword"], tiers:["HT4"]},
+{name:"Notreal", region:"AS", kits:["Mace"], tiers:["LT5"]},
+{name:"Notreal", region:"AS", kits:["NethOP"], tiers:["LT5"]},
+{name:"CriticalClaws", region:"AS", kits:["Mace"], tiers:["LT3"]},
+{name:"Himesh_GG", region:"AS", kits:["NethOP"], tiers:["HT5"]},
+{name:"VanshXA", region:"AS", kits:["Mace"], tiers:["LT5"]},
+{name:"pgp7", region:"AS", kits:["Mace"], tiers:["LT5"]},
+{name:"Streyfe", region:"AS", kits:["smp"], tiers:["LT4"]},
+{name:"InstaBrine", region:"AS", kits:["NethOP"], tiers:["HT4"]},
+];
+
